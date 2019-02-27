832e668f563ccf7b0a83046a73d9901ed8831407
treyfortmuller <tfortmuller@berkeley.edu>
2019-02-27 15:36:04 -0800

    working v1.0

--- tng_chooser.py
+++ tng_chooser.py
@@ -5,9 +5,6 @@ Never suffer from having to choose which episode of
 Star Trek: The Next Generation to watch again...
 
 Trey Fortmuller
-
-Usage:
-TODO
 '''
 
 import random
@@ -20,16 +17,45 @@ def readXML():
 	tree = et.parse(xml_file)
 	root = tree.getroot()
 
+	all_episodes = []
 	for season in root:
+		title_list = []	
+
 		for episode in season:
-			for title in episode:
-				print(title.text, episode.attrib)
+			title = episode[0]
+			title_list.append(title.text)
+		
+		all_episodes.append(title_list)
 
-# TODO: usage, UI, finish choosing
+	return all_episodes
 
-readXML()
+def outputChosen(season, ep):
+	chosen = titles_in_season[ep]
 
-num = random.randint(1, 101) # random int between 1, and 100
+	print("TNG CHOOSER HAS CHOSEN:\n")
+	print("\tSeason " + str(season+1) + ", Episode " + str(ep+1) + ": " + chosen) 
 
-print(num)
+
+all_episodes = readXML()
+print("TNG EPISODE CHOOSER\n")
+
+usecase = input("Enter 1 to return an episode from the whole series.\nEnter 2 to select a season first...\n")
+
+if usecase == 1:
+	# randomly select a season, randomly select an episode, print it
+	rand_season = random.randint(0, 7) # random number between 0 and 6 inclusive
+	titles_in_season = all_episodes[rand_season]
+	rand_ep = random.randint(0, len(titles_in_season))
+
+	outputChosen(rand_season, rand_ep)
+elif usecase == 2:
+	user_select = input("Which season do you want to see?\n")
+	select_season = user_select-1 # to 0 index the choice
+	# randomly select an episode from the selected season, print it
+	titles_in_season = all_episodes[select_season]
+	rand_ep = random.randint(0, len(titles_in_season))
+
+	outputChosen(select_season, rand_ep)
+else:
+	print("That's not an option.")
 	
